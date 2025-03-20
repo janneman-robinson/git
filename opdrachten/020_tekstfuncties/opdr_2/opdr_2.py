cbef6d67cb451b60a1065680a8fb498d48d794fc
Lyam <97109715@st.deltion.nl>
2025-03-20 12:54:14 +0100

    020 opdr2 af

--- opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
+++ opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
@@ -4,6 +4,6 @@
 
 # Hier komt je code...
 
-my_list = ...
-
-print()
+my_list = "Tinus gaat op zijn tandem naar de hottentottententoonstelling"
+aantal_t = my_list.count("t")
+print (f"De letter t komt {aantal_t}x voor in my_string")
