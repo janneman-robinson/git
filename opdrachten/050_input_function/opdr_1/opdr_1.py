b25e77adbeaa641f84070e6e7c4d05edd54f968e
Lyam <97109715@st.deltion.nl>
2025-03-31 11:26:35 +0200

    050 opdr 1 klaar

--- opdrachten/050_input_function/opdr_1/opdr_1.py
+++ opdrachten/050_input_function/opdr_1/opdr_1.py
@@ -5,3 +5,18 @@
 # Hier komt je code, maak gebruik van de input functie om de lengte van de rechthoekzijden van de driehoek op te vragen.
 
 
+lengte_van_zijde_1 = 6
+lengte_van_zijde_2 = 4
+
+kwadraat_zijde_1 = lengte_van_zijde_1 ** 2
+kwadraat_zijde_2 = lengte_van_zijde_2 ** 2
+
+kwadraat_lengte_van_schuine_zijde = kwadraat_zijde_1 + kwadraat_zijde_2
+
+lengte_van_schuine_zijde = kwadraat_lengte_van_schuine_zijde ** 0.5
+
+
+
+print("lengte van zijde 1:", lengte_van_zijde_1)
+print("lengte van zijde 2:" , lengte_van_zijde_2)
+print("lengte van schuine zijde:" , lengte_van_schuine_zijde)
