706a11874bb8c72b33ab0427d827294966dadca8
Lyam <97109715@st.deltion.nl>
2025-04-02 14:33:01 +0200

    050 opdr 2 klaar

--- opdrachten/050_input_function/opdr_2/opdr_2.py
+++ opdrachten/050_input_function/opdr_2/opdr_2.py
@@ -4,4 +4,15 @@
 
 # Hier komt je code...
 
-gasten = ["Jij", ... ]
+# Maak de gastenlijst
+lijst = ["Jij", "Paul", "Kees", "Marie", "Hilda"]
+print("Oorspronkelijke lijst:", lijst)
+
+# Marie belt af
+lijst.remove("Marie")
+print("Na afmelding van Marie:", lijst)
+
+# George wil naast Kees zitten
+index_kees = lijst.index("Kees")
+lijst.insert(index_kees + 1, "George")
+print("Na toevoeging van George:", lijst)
