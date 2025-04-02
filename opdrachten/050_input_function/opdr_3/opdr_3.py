ff3c31b999657045c15bda70d5e97a03618e654d
Lyam <97109715@st.deltion.nl>
2025-04-02 14:45:48 +0200

    050 opdr 3 klaar

--- opdrachten/050_input_function/opdr_3/opdr_3.py
+++ opdrachten/050_input_function/opdr_3/opdr_3.py
@@ -1,6 +1,12 @@
 # Opdracht 3 input functie
 # Naam student:
 # Groep:
+from operator import truediv
 
 # Hier komt je code...
 
+lijst = ["Amsterdam", "Zwolle", "Dronten", "Haarlem", "Zaanstad"]
+gesorteerd = sorted(lijst)
+
+gesorteerd.reverse()
+print (gesorteerd)
