4d27fc9d8899446e95531fbbbc8a3560678f03cb
Lyam <97109715@st.deltion.nl>
2025-04-07 09:26:08 +0200

    mini rpg project begonnon

---
+++ projecten/mini-rpg-lyam.py
@@ -0,0 +1,9 @@
+print("Je wordt wakker in een donker bos...")
+keuze = input("Ga je naar het (1) pad of (2) grot? ")
+
+if keuze == "1":
+    print("Je volgt het pad en vindt een hut.")
+elif keuze == "2":
+    print("Je loopt de grot in... en hoort iets grommen.")
+else:
+    print("Je blijft stil staan. Iets komt dichterbij...")
