57c13b019034e355eea6f61a20e6a65c35a5d3c3
Lyam <97109715@st.deltion.nl>
2025-04-07 09:59:09 +0200

    mini rpg progressie

--- projecten/mini-rpg-lyam.py
+++ projecten/mini-rpg-lyam.py
@@ -1,9 +1,14 @@
-print("Je wordt wakker in een donker bos...")
-keuze = input("Ga je naar het (1) pad of (2) grot? ")
+print("\033[44mJe wordt wakker in een donker bos...\033[0m")
+keuze = input("\033[43mGa je naar het (1) pad of (2) grot?\033[0m")
 
 if keuze == "1":
-    print("Je volgt het pad en vindt een hut.")
+    print("\033[44mJe volgt het pad en vindt een verlaten hut.\033[0m")
+    keuze_pad = input("\033[43mna het betreden van de hut vind je een (1) deur naar de kelder en een (2) mysterieuze soep die erg lekker ruikt.\033[0m")
+    if keuze_pad == "1":
+        print("\033[43m...\033[0m")
+    elif keuze_pad == "2":
+        print("\033[43mje neemt een slokje van de soep, en je ontploft.\033[0m")
 elif keuze == "2":
-    print("Je loopt de grot in... en hoort iets grommen.")
+    print("\033[44mJe loopt de grot in... en hoort iets grommen.\033[0m")
 else:
-    print("Je blijft stil staan. Iets komt dichterbij...")
+    print("\033[44mJe blijft stil staan. Iets komt dichterbij...\033[0m")
