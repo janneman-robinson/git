0d6574b6b284017f4a94cd319bc25144c000da9a
Lyam <97109715@st.deltion.nl>
2025-04-09 15:10:34 +0200

    project bijna af

--- projecten/mini-rpg-lyam.py
+++ projecten/mini-rpg-lyam.py
@@ -7,14 +7,13 @@ print("\033[32m╚══════╝░░░╚═╝░░░╚═╝░�
 
 print("\033[32mWelkom bij de Mini RPG!\033[0m")
 print("\033[32mBij een keuze typ je 1, 2, 3, etc. om je keuze te maken.\033[0m")
-print("\033[32mJe kan een keuze ook overslaan door op ENTER te drukken.\033[0m")
 print("\033[32mVeel succes!\033[0m")
 
 import time
 time.sleep(5)
 
-print("\033[33mJe wordt wakker in een donker bos... voor je vind je een duister pad, en achter je een grot met een paar touwen.\033[0m")
-keuze = input("\033[43mBetreed het (1) duistere pad of (2) grot?\033[0m")
+print("\033[33mJe wordt wakker, en word omringt door een donker bos... voor je vind je een duister pad, en achter je een grot met een paar touwen.\033[0m")
+keuze = input("\033[43mBetreed het (1) duistere pad, duik in de (2) grot, of spook rond in het (3) donkere bos?\033[0m")
 
 if keuze == "1":
     print("\033[33mJe volgt het pad en vindt een verlaten houten hut.\033[0m")
@@ -22,7 +21,7 @@ if keuze == "1":
 
     if keuze_pad == "1":
         print("\033[33mHet luik kraakt terwijl je hem opent, er komt een groene gloed uit de opening, in de kelder staat een heks, ze heeft 2 pillen in haar hand.\033[0m")
-        keuze_luik = input("\033[43mNeem de (1) rode pil, of neem de (2) blauwe pil.\033[0m")
+        keuze_luik = input("\033[43mNeem de (1) rode pil, of neem de (2) blauwe pil... of (3) denk er nog even over na...\033[0m")
 
         if keuze_luik == "1":
             print("\033[33mJe neemt de rode pil, na enkele seconden zie je de waarheid van het universum, en dat de wereld bestaat uit binaire code. je bent gestegen boven het menselijk bestaan.\033[0m")
@@ -32,7 +31,7 @@ if keuze == "1":
             print("\033[33mJe neemt de blauwe pil, er lijkt eerst niks te gebeuren, maar voordat je iets kunt zeggen wordt je wat licht in je hoofd... je wordt wakker in een donker bos...\033[0m")
             time.sleep(2)
             print("\033[31mEinde 2: Loop\033[0m")
-        else:
+        elif keuze_luik == "3":
             print("\033[33mde keuze duurde te lang, de heks keek je boos aan en propte uit woede beide pillen in je mond, de heks lachte terwijl je zelf ook langzaam in een heks veranderde\033[0m")
             time.sleep(2)
             print("\033[31mEinde 3: Heks \033[0m")
@@ -56,22 +55,51 @@ elif keuze == "2":
         print("\033[33mje loopt naar de zak met goud, er is niemand die de zak bewaakt, je kan hem meenemen, of natuurlijk laten staan.\033[0m")
         keuze_zak = input("\033[43m(1) neem de zak mee, of (2) laat hem staan.")
         if keuze_zak == "1":
-            print("\033[33m'Net als je de zak op wil pakken springt er een goblin tussen het goud vandaan.\033[0m")
+            print("\033[33mNet als je de zak op wil pakken springt er een goblin tussen het goud vandaan.\033[0m")
             print("\033[33m'Mijn naam is kevin de goblin, en dit goud is van mij! je mag de zak best hebben, maar alleen als je wat vragen juist weet te beantwoorden!'\033[0m")
             print("\033[33m'Vraag 1: wie schilderde de Mona Lisa?\033[0m")
             keuze_vraag_1 = input("\033[43m(1) leonardo dicaprio, (2) leonardo da vinci, of was het (3) leonardo vibonacci?\033[0m")
             if keuze_vraag_1 == "1":
                 print("\033[33m'DAT IS FOUT MUAHAHAHH, DEZE ZAK GOUD BLIJFT LEKKER VAN MIJ\033[0m")
                 time.sleep(2)
-                print("\033[31mEinde 7: Fout andwoord :(\033[0m")
+                print("\033[31mEinde 7: Fout antwoord, stink goblin\033[0m")
             elif keuze_vraag_1 == "2":
-                print()
+                print("\033[33m'Helemaal goed! ik ben onder dr indruk!'\033[0m")
+                print("\033[33m'Vraag 2: hoe heet een organisme dat vlees eet?'\033[0m")
+                keuze_vraag_2 = input("\033[43m(1) een omnivoor, (2) een herbivoor, of een (3) carnivoor?\033[0m")
+                if keuze_vraag_2 == "1":
+                    print("\033[33m'DAT IS FOUT MUAHAHAHH, DEZE ZAK GOUD BLIJFT LEKKER VAN MIJ'\033[0m")
+                    time.sleep(2)
+                    print("\033[31mEinde 7: Fout antwoord, stink goblin\033[0m")
+                elif keuze_vraag_2 == "2":
+                    print("\033[33m'DAT IS FOUT MUAHAHAHH, DEZE ZAK GOUD BLIJFT LEKKER VAN MIJ'\033[0m")
+                    time.sleep(2)
+                    print("\033[31mEinde 7: Fout antwoord, stink goblin\033[0m")
+                elif keuze_vraag_2 == "3":
+                    print("\033[33m'oh wauw, geniaal!'\033[0m")
+                    print("\033[33m'Vraag 3, dit is de laatste vraag: wat is de 2e letter van het griekse alfabet?'\033[0m")
+                    keuze_vraag_3 = input("\033[43mIs het (1) beta, (2) gamma, of (3) alpha?\033[0m")
+                    if keuze_vraag_3 == "1":
+                        print("\033[33m'zo, zo, jij bent zeker de slimste thuis, ik had niet verwacht dat iemand ooit langs mijn algemene kennis vragen zou komen, je hebt de zak goud helemaal verdient!'\033[0m")
+                        print("\033[33mJe loopt voldaan de grot uit, met een lach op je gezicht, en 20 kilo 24 karaat goud ter waarde van 1729400 euro ben je eindelijk rijk genoeg om je eerste huis te kopen!\033[0m")
+                        time.sleep(2)
+                        print("\033[32mEinde 8: gewonnen in het leven.\033[0m")
 
-
-
-else:
-    print("\033[33mJe blijft net iets te lang stil staan. Iets komt dichterbij...\033[0m")
-    print("\033[33mEr verschijnt een bigfoot voor je! wat ga je doen?\033[0m")
+                    elif keuze_vraag_3 == "2":
+                        print("\033[33m'DAT IS FOUT MUAHAHAHH, DEZE ZAK GOUD BLIJFT LEKKER VAN MIJ'\033[0m")
+                        time.sleep(2)
+                        print("\033[31mEinde 7: Fout antwoord, stink goblin\033[0m")
+                    elif keuze_vraag_3 == "3":
+                        print("\033[33m'DAT IS FOUT MUAHAHAHH, DEZE ZAK GOUD BLIJFT LEKKER VAN MIJ'\033[0m")
+                        time.sleep(2)
+                        print("\033[31mEinde 7: Fout antwoord, stink goblin\033[0m")
+            elif keuze_vraag_1 == "3":
+                print("\033[33m'DAT IS FOUT MUAHAHAHH, DEZE ZAK GOUD BLIJFT LEKKER VAN MIJ'\033[0m")
+                time.sleep(2)
+                print("\033[31mEinde 7: Fout antwoord, stink goblin\033[0m")
+elif keuze == "3":
+    print("\033[33mTerwijl je rondspookt door het bos hoor je wat in de struiken... Iets komt dichterbij...\033[0m")
+    print("\033[33mOpeens verschijnt er een bigfoot voor je! wat ga je doen?\033[0m")
     keuze_bigfoot = input("\033[43mje (1) Slaat bigfoot op zijn hoofd met een stok, of kan je toch beter (2) wegrennen...\033[0m")
 
     if keuze_bigfoot == "1":
