62528d2fcaf34d12227ba06b46c5a7655feb45bc
Lyam <97109715@st.deltion.nl>
2025-04-09 09:42:03 +0200

    095 af

--- opdrachten/095_modules/opdr_1/opdr_1.py
+++ opdrachten/095_modules/opdr_1/opdr_1.py
@@ -3,3 +3,10 @@
 # Groep:
 
 # importeer de module csv...
+import csv
+
+def lees_csv(bestandsnaam):
+    with open(bestandsnaam, mode='r') as bestand:
+        csv_reader = csv.reader(bestand)
+        for rij in csv_reader:
+            print(rij)
--- opdrachten/095_modules/opdr_2/opdr_2.py
+++ opdrachten/095_modules/opdr_2/opdr_2.py
@@ -5,3 +5,27 @@
 # import .....
 # for line in open("test.csv", 'rt'):
 #   jouw code komt hier!
+
+from my_modules import csv
+
+# Voorbeeldlijst van personen
+personen = [
+    {"voornaam": "Jan", "tussenvoegsel": "Van Der", "achternaam": "Vliet"},
+    {"voornaam": "Jan", "tussenvoegsel": "Jaap", "achternaam": "Siewers"},
+    {"voornaam": "Pieter", "tussenvoegsel": "de", "achternaam": "Boer"},
+    {"voornaam": "Piet", "tussenvoegsel": "", "achternaam": "Kramer"},
+    {"voornaam": "Pie", "tussenvoegsel": "", "achternaam": "Wijnands"}
+]
+
+# Functie om te filteren op basis van het begin van een veld
+def filter(persoon_lijst, filterveld, filter):
+    for persoon in persoon_lijst:
+        if persoon[filterveld].startswith(filter):
+            print(f"{persoon['voornaam']} {persoon['tussenvoegsel']} {persoon['achternaam']}")
+
+# Gebruik de filterfunctie om te zoeken op basis van de voornaam
+print("Filter op voornaam met 'ja':")
+filter(personen, "voornaam", "Ja")
+
+print("\nFilter op voornaam met 'Pie':")
+filter(personen, "voornaam", "Pie")
